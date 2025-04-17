f64bc325d6a55bc7cee5c6b89157d069068e3851
Maeda Toshiaki <toshiaki.maeda@supership.jp>
2025-04-17 11:42:01 +0900

    whichwrap

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -45,6 +45,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        whichwrap = "b,s,h,l,<,>,[,]",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
